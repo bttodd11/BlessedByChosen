4d371054d766344fb8b497b03083611dd6941dec
bttodd11 <bttodd@icloud.com>
2020-06-07 09:01:16 -0500

    Updates

---
+++ precache-manifest.cff7cff764f0e0f385c6f8ddaeb8f8af.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1119be6c78fb3df53835e110cf9e7305",
+    "url": "/BlessedByChosen/my-app/index.html"
+  },
+  {
+    "revision": "2a2fb9a8c77ca730d092",
+    "url": "/BlessedByChosen/my-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ba94eccc10062ddd4fa6",
+    "url": "/BlessedByChosen/my-app/static/css/main.8bee98d0.chunk.css"
+  },
+  {
+    "revision": "2a2fb9a8c77ca730d092",
+    "url": "/BlessedByChosen/my-app/static/js/2.1e8b8e50.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/BlessedByChosen/my-app/static/js/2.1e8b8e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba94eccc10062ddd4fa6",
+    "url": "/BlessedByChosen/my-app/static/js/main.575e5f7b.chunk.js"
+  },
+  {
+    "revision": "9b7fad37ae778f41f2cb",
+    "url": "/BlessedByChosen/my-app/static/js/runtime-main.59fa7b3c.js"
+  },
+  {
+    "revision": "6a021f9c96b8fce72535d564226a0fbd",
+    "url": "/BlessedByChosen/my-app/static/media/backGroundImage.6a021f9c.jpg"
+  },
+  {
+    "revision": "a5bff11c2af875b6e54297d0d360406e",
+    "url": "/BlessedByChosen/my-app/static/media/backGroundsm.a5bff11c.jpg"
+  },
+  {
+    "revision": "af8fcfd2dc65ef0907c0c5f40b2ce453",
+    "url": "/BlessedByChosen/my-app/static/media/bgLandscape.af8fcfd2.jpg"
+  }
+]);
