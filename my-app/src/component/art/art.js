f67a542d147d7e9168669c36afd2215b1c3eee03
bttodd11 <bttodd@icloud.com>
2020-06-21 22:19:52 -0500

    Worked on CSS props

--- my-app/src/component/art/art.js
+++ my-app/src/component/art/art.js
@@ -6,6 +6,16 @@ const art = () => {
     <div class="artSection" id="collections">
 <h1 class="collectionText">Collections</h1>
 <div class="container collectionSection">
+  <div class="collectionPassage">
+    <p class="">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Aliquam in arcu ut nulla sollicitudin rutrum in quis lacus. 
+    Etiam vitae semper urna. Suspendisse rutrum egestas ullamcorper. 
+    Sed non justo et nunc venenatis convallis. Proin dictum felis non dui tincidunt,
+     vel pretium justo tincidunt. Phasellus finibus nunc in ornare scelerisque. Nullam pretium fermentum diam, et auctor eros lacinia et. Phasellus 
+     vehicula urna eu arcu iaculis ultricies. Fusce consequat, metus a interdum faucibus, odio mauris cursus ipsum, at lacinia est ipsum eget augue. Donec dignissim risus nec purus fermentum, quis commodo purus dictum. Donec in ligula id libero condimentum commodo. Ut hendrerit nibh in rutrum molestie.
+    </p>
+  </div>
   <div class="row">
     <div class="col-sm">
       <h2 class="seriesText">Home Base Series</h2>
--- my-app/src/component/title/title.js
+++ my-app/src/component/title/title.js
@@ -10,9 +10,9 @@ const title = () => {
         <h1 className="title"> Chosen</h1>
         <nav class="navbar navbar-light bg-light ">
           <a class="navbar-brand mx-auto" href="#">
-          <img title="navIcon" src={Facebook} alt="my-img" class="icons mr-5" />
-          <img title="navIcon" src={Instagram} alt="my-img" class="icons" />
-          <img title="navIcon" src={Email} alt="my-img" class="icons ml-5" />
+         <a href="https://www.facebook.com/profile.php?id=605890961"><img title="navIcon" src={Facebook} alt="my-img" class="icons mr-5" /> </a>
+         <a href="https://www.instagram.com/ericthechosen/"> <img title="navIcon" src={Instagram} alt="my-img" class="icons" /> </a>
+         <a href=""><img title="navIcon" src={Email} alt="my-img" class="icons ml-5" /> </a>
           </a>
 </nav>
     </div>
