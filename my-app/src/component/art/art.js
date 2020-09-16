97e45576cdec36996b53cdf532c8abfaf6d15e44
Brian <BTodd@Brians-MacBook-Pro.local>
2020-09-15 23:09:37 -0500

    Added album cover and font and adjusted CSS props

--- my-app/src/component/art/art.js
+++ my-app/src/component/art/art.js
@@ -7,28 +7,12 @@ const art = () => {
 <h1 class="collectionText">Collections</h1>
 <div class="container collectionSection">
   <div class="collectionPassage">
-    <p class="">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Aliquam in arcu ut nulla sollicitudin rutrum in quis lacus. 
-    Etiam vitae semper urna. Suspendisse rutrum egestas ullamcorper. 
-    Sed non justo et nunc venenatis convallis. Proin dictum felis non dui tincidunt,
-     vel pretium justo tincidunt. Phasellus finibus nunc in ornare scelerisque. Nullam pretium fermentum diam, et auctor eros lacinia et. Phasellus 
-     vehicula urna eu arcu iaculis ultricies. Fusce consequat, metus a interdum faucibus, odio mauris cursus ipsum, at lacinia est ipsum eget augue. Donec dignissim risus nec purus fermentum, quis commodo purus dictum. Donec in ligula id libero condimentum commodo. Ut hendrerit nibh in rutrum molestie.
+    <p class="passageText">
+    Every piece is 1 of 1, <br />
+    Every collection is 1 of None
     </p>
   </div>
   <div class="row">
-    <div class="col-sm">
-      <h2 class="seriesText">Home Base Series</h2>
-      One of three columns
-    </div>
-    <div class="col-sm">
-      <h2 class="seriesText">Gif Collection</h2>
-      One of three columns
-    </div>
-    <div class="col-sm">
-      <h2 class="seriesText">Gif Collection</h2>
-      One of three columns
-    </div>
   </div>
 </div>
     </div>
--- my-app/src/component/music/music.js
+++ my-app/src/component/music/music.js
@@ -1,12 +1,17 @@
 import React from "react";
+import AlbumCover from './img/albumCover.JPG';
 import './music.css';
 
 const music = () => {
     return (
       <div id="music" class="musicSection">
-        <h1 class="musicTitle">Tunnel Vision</h1>
+        <h1 class="musicTitle">Time Will Tell (EP)</h1>
         <div class="musicContent">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/F-PDQT0lVNw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <img class="albumCover"  src={AlbumCover} />
+        </div>
+        <h1 class="musicTitle tunnelVision">Tunnel Vision</h1>
+        <div class="musicContent">
+          <iframe width="760" height="315" src="https://www.youtube.com/embed/F-PDQT0lVNw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
       </div>
     )
