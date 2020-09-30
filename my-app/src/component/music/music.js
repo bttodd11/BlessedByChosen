bef1f20c14431feab9e3410a5924907f39e9017d
Brian <BTodd@Brians-MacBook-Pro.local>
2020-09-29 22:44:24 -0500

    Added Album cover

--- my-app/src/component/music/music.js
+++ my-app/src/component/music/music.js
@@ -9,11 +9,7 @@ const music = () => {
         <div class="musicContent">
       <img class="albumCover"  src={AlbumCover} />
         </div>
-        <div class="linkSection">
-          <p class="linkDownload">
-            Download 
-          </p>
-        </div>
+       <a href=""> <button class="linkSection" href="www.google.com">Download</button></a>
         <h1 class="musicTitle tunnelVision">Tunnel Vision</h1>
         <div class="musicContent">
           <iframe width="760" height="315" src="https://www.youtube.com/embed/F-PDQT0lVNw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
