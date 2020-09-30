736986c35f10a00613d7c84f9b0380236124c37a
Brian <BTodd@Brians-MacBook-Pro.local>
2020-09-29 23:25:58 -0500

    Updated CSS props

--- my-app/src/component/music/music.js
+++ my-app/src/component/music/music.js
@@ -10,9 +10,11 @@ const music = () => {
       <img class="albumCover"  src={AlbumCover} />
         </div>
        <a href=""> <button class="linkSection" href="www.google.com">Download</button></a>
+       <div class="videoSection">
         <h1 class="musicTitle tunnelVision">Tunnel Vision</h1>
         <div class="musicContent">
           <iframe width="760" height="315" src="https://www.youtube.com/embed/F-PDQT0lVNw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
         </div>
       </div>
     )
